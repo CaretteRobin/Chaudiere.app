e29776e0c19cb705467d062cad80466be604900a
Paul5400 <paul.andrieu54@gmail.com>
2025-06-11 11:09:07 +0200

    feat(event): remove description from Event model and update master_details layout

--- lib/core/models/event.dart
+++ lib/core/models/event.dart
@@ -2,13 +2,11 @@ class Event {
   final String title;
   final String category;
   final String startDate;
-  final String description;
 
   Event({
     required this.title,
     required this.category,
     required this.startDate,
-    required this.description,
   });
 
   factory Event.fromJson(Map<String, dynamic> json) {
@@ -16,7 +14,6 @@ class Event {
       title: json['title'] ?? '',
       category: json['category'] ?? '',
       startDate: json['start_date'] ?? '',
-      description: json['description'] ?? '',
     );
   }
 }
